8361d5ee6da93875fb75fdf418766064dc49e03e
Yuri Fernandes <fernandesyuri104@gmail.com>
2025-05-12 18:11:23 -0300

    Atualizando os arquivos e urls das imagens.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,62 @@
 const fundoSombra = document.querySelector(".sombra");
 const modalCartas = document.querySelector(".modal-cartas");
+const cartasElementos = document.querySelectorAll(".cartas");
+
 const btnVerModal = document.querySelector(".btnVisualizarCartas");
 const btnFecharModal = document.querySelector(".btnFechar");
 const btnJogar = document.querySelector(".btnJogar");
+
+let cartas = [
+    {
+        id: 1,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    },
+    
+    {
+        id: 2,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    },
+
+    {
+        id: 3,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    },
+
+    {
+        id: 4,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    },
+
+    {
+        id: 5,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    },
+
+    {
+        id: 6,
+        nome: undefined,
+        tipo: undefined,
+        ataque: 0,
+        defesa: 0
+    }
+
+];
+console.log(cartas);
 
 btnVerModal.addEventListener("click", ()=>{
     exibirModalCartas();
@@ -18,11 +72,19 @@ btnJogar.addEventListener("click", ()=>{
 });
 
 function exibirModalCartas(){
+    modalCartas.classList.remove("desativado"); 
     modalCartas.showModal();
-    fundoSombra.style.display = "block";
+    
+    modalCartas.classList.add("ativo");
+    fundoSombra.classList.add("ativo");
 };
 
 function esconderModalCartas(){
-    modalCartas.close();
-    fundoSombra.style.display = "none";
+    modalCartas.classList.remove("ativo"); 
+    modalCartas.classList.add("desativado"); 
+    
+    setTimeout(() => {
+        modalCartas.close(); 
+        fundoSombra.classList.remove("ativo");
+    }, 300); 
 };
